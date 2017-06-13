3eb0a418b9fda8064ad8cd870026040f12d5c411
chenghao01 <chtly666@163.com>
2017-06-13 16:03:29 +0800

    压缩上线代码

---
+++ build/6.2d677.js
@@ -0,0 +1,1 @@
+webpackJsonp([6],{286:function(e,t,u){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var n=u(5),l=function(e){return e&&e.__esModule?e:{default:e}}(n),r=function(){return l.default.createElement("div",null,"detail")};t.default=r}});
